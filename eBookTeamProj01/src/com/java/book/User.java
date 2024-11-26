691675439695bedddcf4af262dd7bbb800ff0f62
cheowha1 <cheowha1@naver.com>
2024-11-26 15:50:17 +0900

    [현석]User

--- eBookTeamProj01/src/com/java/book/User.java
+++ eBookTeamProj01/src/com/java/book/User.java
@@ -2,6 +2,67 @@ package com.java.book;
 
 public class User {
 	private int uid;
-	private int user_id;
-	private String user_name;
+	private int userId;
+	private int userPassword;
+	private String adress;
+	private int phoneNumber;
+	private String email;
+	private String admin;
+	public int getUid() {
+		return uid;
+	}
+	
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public int getUserPassword() {
+		return userPassword;
+	}
+	
+	public void setUserPassword(int userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	public String getAdress() {
+		return adress;
+	}
+	
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getAdmin() {
+		return admin;
+	}
+	
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+	
+	
 }
