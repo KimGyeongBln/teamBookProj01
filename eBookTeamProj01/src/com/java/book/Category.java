d9679cf1b4195d2fda02ac8d0bd2a30248d85a0c
KimGyeongBln <hhk3950@naver.com>
2024-11-26 15:46:42 +0900

    [경빈]Category 클래스 추가

---
+++ eBookTeamProj01/src/com/java/book/Category.java
@@ -0,0 +1,22 @@
+package com.java.book;
+
+public class Category {
+	private int bookId;
+	private int categoryId;
+	
+	public int getBookId() {
+		return bookId;
+	}
+	
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	
+	public int getCategoryId() {
+		return categoryId;
+	}
+	
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+}
