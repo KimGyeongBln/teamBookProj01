b047ac6be45e6e74309d4279fb3fa735d1cb8f75
cheowha1 <cheowha1@naver.com>
2024-11-27 14:58:46 +0900

    [현석] BookDao,BookDaoImpl 수정
    BookDaoImpl 검색에 출판사 검색, 카테고리 검색 추가
    BookDao 추가

--- eBookTeamProj01/src/com/java/book/BookDao.java
+++ eBookTeamProj01/src/com/java/book/BookDao.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface BookDao {
 	public List<BookVo> getList(); // 전체 책 목록
 	public List<BookVo> search(String keyword); //@dhkim: 일부 문자로 책을 검색할 수 있는 기능
+	public List<BookVo> searchPublisher(String pulisher); //@hsNa: 저자 검색
+	public List	<BookVo> searchcategoryId(String category_id); //@hsNa: 카테고리 검색
 	public BookVo get(int bookId);		// 리스트에서 객체 가져오기
 	public boolean insert(BookVo vo); 	// 추가
 	public boolean update(BookVo vo); 	// 수정
